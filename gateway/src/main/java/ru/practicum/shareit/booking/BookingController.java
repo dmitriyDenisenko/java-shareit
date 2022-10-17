26fe00eb753d2d0c17b989026a36a6ca43dfa2bc
dmitriyDenisenko <dipster86@yandex.ru>
2022-10-17 14:18:57 +0300

    remove error used

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -10,8 +10,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
-import ru.practicum.shareit.booking.exception.BookingDtoBadStateException;
-import ru.practicum.shareit.booking.exception.ErrorResponse;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
