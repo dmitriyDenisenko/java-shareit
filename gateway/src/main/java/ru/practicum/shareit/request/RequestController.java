44fd2f0a35b351d9d80bfc66f464e75f1c397ff2
dmitriyDenisenko <dipster86@yandex.ru>
2022-10-17 15:05:35 +0300

    errorHandler

--- gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestController.java
@@ -2,12 +2,15 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(path = "/requests")
@@ -34,6 +37,9 @@ public class RequestController {
     public ResponseEntity<Object> getAll(@RequestHeader("X-Sharer-User-Id") long userId,
                                          @Valid @RequestParam(name = "from", defaultValue = "1") int from,
                                          @RequestParam(name = "size", defaultValue = "10") int size) {
+        if(from < 0){
+            throw new IllegalArgumentException();
+        }
         log.info("get requests all");
         return requestClient.getAll(userId, from, size);
     }
@@ -43,5 +49,13 @@ public class RequestController {
                                           @PathVariable long requestId) {
         log.info("get request id = {}", requestId);
         return requestClient.getById(userId, requestId);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<Map<String, String>> errorHandler(IllegalArgumentException ex) {
+        Map<String, String> resp = new HashMap<>();
+        resp.put("error", String.format("Unknown state: UNSUPPORTED_STATUS"));
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
     }
 }
